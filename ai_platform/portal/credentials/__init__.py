2ccf2388c35d8eece83e272fed1a362d8fab9ce4
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-28 22:20:02 +0200

    feat(portal): expose PI-07 credential broker package

---
+++ ai_platform/portal/credentials/__init__.py
@@ -0,0 +1,57 @@
+from ai_platform.portal.credentials.broker import VaultCredentialBroker
+from ai_platform.portal.credentials.errors import (
+    CredentialBrokerError,
+    CredentialIsolationError,
+    CredentialPolicyError,
+    CredentialRevokedError,
+    CredentialRotationRequiredError,
+    CredentialUnavailableError,
+    VaultAuthenticationError,
+    VaultProtocolError,
+    VaultTransportError,
+)
+from ai_platform.portal.credentials.material import (
+    CredentialMaterial,
+    ResolvedCredentialLease,
+)
+from ai_platform.portal.credentials.schema import (
+    CredentialLeaseEvidence,
+    CredentialLeaseRequest,
+    CredentialPurpose,
+    VaultCredentialMetadata,
+)
+from ai_platform.portal.credentials.vault import (
+    HttpxVaultTransport,
+    VaultAppRoleClient,
+    VaultAppRoleConfig,
+    VaultCredentialDocument,
+    VaultCredentialRecord,
+    VaultHttpTransport,
+    validate_private_https_endpoint,
+)
+
+__all__ = [
+    "CredentialBrokerError",
+    "CredentialIsolationError",
+    "CredentialLeaseEvidence",
+    "CredentialLeaseRequest",
+    "CredentialMaterial",
+    "CredentialPolicyError",
+    "CredentialPurpose",
+    "CredentialRevokedError",
+    "CredentialRotationRequiredError",
+    "CredentialUnavailableError",
+    "HttpxVaultTransport",
+    "ResolvedCredentialLease",
+    "VaultAppRoleClient",
+    "VaultAppRoleConfig",
+    "VaultAuthenticationError",
+    "VaultCredentialBroker",
+    "VaultCredentialDocument",
+    "VaultCredentialMetadata",
+    "VaultCredentialRecord",
+    "VaultHttpTransport",
+    "VaultProtocolError",
+    "VaultTransportError",
+    "validate_private_https_endpoint",
+]
